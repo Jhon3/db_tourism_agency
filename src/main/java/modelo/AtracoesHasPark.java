ef6c0abb21874c4012dc62861272f02695c82860
Home <Home@Home-PC>
2018-11-24 16:19:03 -0300

    "inicio do projeto: criando o conreoller"

---
+++ src/main/java/modelo/AtracoesHasPark.java
@@ -0,0 +1,21 @@
+package modelo;
+
+public class AtracoesHasPark {
+	private Atracoes atraoes;
+	private Park park;
+	public Atracoes getAtraoes() {
+		return atraoes;
+	}
+	public void setAtraoes(Atracoes atraoes) {
+		this.atraoes = atraoes;
+	}
+	public Park getPark() {
+		return park;
+	}
+	public void setPark(Park park) {
+		this.park = park;
+	}
+	
+	
+
+}
---
+++ src/main/java/modelo/RestauranteHasPark.java
@@ -0,0 +1,21 @@
+package modelo;
+
+public class RestauranteHasPark {
+	
+	private Restaurantes restaurantes;
+	private Park park;
+	
+	public Restaurantes getRestaurantes() {
+		return restaurantes;
+	}
+	public void setRestaurante(Restaurantes restaurantes) {
+		this.restaurantes = restaurantes;
+	}
+	public Park getPark() {
+		return park;
+	}
+	public void setPark(Park park) {
+		this.park = park;
+	}
+
+}
